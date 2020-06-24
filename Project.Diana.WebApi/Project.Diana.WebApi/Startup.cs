62b34d71057d38d7c8f259b9e0d26ae94ba623f3
Dennis D. Silva <dsilva609@gmail.com>
2020-06-23 22:22:54 -0400

    project(startup): add simpleinjector

--- Project.Diana.WebApi/Project.Diana.WebApi/Startup.cs
+++ Project.Diana.WebApi/Project.Diana.WebApi/Startup.cs
@@ -3,11 +3,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SimpleInjector;
 
 namespace Project.Diana.WebApi
 {
     public class Startup
     {
+        private readonly Container _container = new Container();
         public IConfiguration Configuration { get; }
 
         public Startup(IConfiguration configuration) => Configuration = configuration;
@@ -20,19 +22,30 @@ namespace Project.Diana.WebApi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            app.UseHttpsRedirection()
+               .UseRouting()
+               .UseAuthorization()
+               .UseEndpoints(endpoints =>
+               {
+                   endpoints.MapControllers();
+               })
+               .UseSimpleInjector(_container);
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            _container.Verify();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) => services.AddControllers();
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSimpleInjector(_container);
+
+            //--TODO: pull out individual registrations
+            InitializeContainer();
+        }
+
+        private void InitializeContainer()
+        {
+        }
     }
 }
