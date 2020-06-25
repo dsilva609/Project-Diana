0b7060695be665ab7837a291b790357461e659a4
Dennis D. Silva <dsilva609@gmail.com>
2020-06-24 22:22:03 -0400

    feat(wish): start wish query

---
+++ Project.Diana.Data.Tests/Features/Wish/Queries/WishGetByIDQueryTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentAssertions;
+using Project.Diana.Data.Features.Wish.Queries;
+using Xunit;
+
+namespace Project.Diana.Data.Tests.Features.Wish.Queries
+{
+    public class WishGetByIDQueryTests
+    {
+        [Fact]
+        public void QueryThrowsWhenUserIDIsDefault()
+        {
+            Action createWithDefaultUserID = () => new WishGetByIDQuery(0, 1);
+
+            createWithDefaultUserID.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void QueryThrowsWhenWishIDIsDefault()
+        {
+            Action createWithDefaultWishID = () => new WishGetByIDQuery(1, 0);
+
+            createWithDefaultWishID.Should().Throw<ArgumentException>();
+        }
+    }
+}
---
+++ Project.Diana.Data/Features/Wish/Queries/WishGetByIDQuery.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.GuardClauses;
+
+namespace Project.Diana.Data.Features.Wish.Queries
+{
+    public class WishGetByIDQuery
+    {
+        public int UserID { get; }
+        public int WishId { get; }
+
+        public WishGetByIDQuery(int userID, int wishID)
+        {
+            Guard.Against.Default(userID, nameof(userID));
+            Guard.Against.Default(wishID, nameof(wishID));
+
+            UserID = userID;
+            WishId = wishID;
+        }
+    }
+}
