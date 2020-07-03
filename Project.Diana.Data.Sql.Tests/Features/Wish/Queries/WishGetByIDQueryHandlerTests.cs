da56b0885eb3fb1c1b468c098017f34b582a4fef
Dennis D. Silva <dsilva609@gmail.com>
2020-07-02 23:44:40 -0400

    test(sql): add sqlite test base

--- Project.Diana.Data.Sql.Tests/Features/Wish/Queries/WishGetByIDQueryHandlerTests.cs
+++ Project.Diana.Data.Sql.Tests/Features/Wish/Queries/WishGetByIDQueryHandlerTests.cs
@@ -1,36 +1,49 @@
 ﻿using System.Threading.Tasks;
 using AutoFixture;
 using FluentAssertions;
-using Moq;
+using Project.Diana.Data.Features.Wish;
 using Project.Diana.Data.Features.Wish.Queries;
 using Project.Diana.Data.Sql.Context;
 using Project.Diana.Data.Sql.Features.Wish.Queries;
+using Project.Diana.Tests.Common.TestBases;
 using Xunit;
 
 namespace Project.Diana.Data.Sql.Tests.Features.Wish.Queries
 {
-    public class WishGetByIDQueryHandlerTests
+    public class WishGetByIDQueryHandlerTests : DbContextTestBase<ProjectDianaContext>
     {
         private readonly WishGetByIDQueryHandler _handler;
-        private readonly Mock<IProjectDianaContext> _projectDianaContext;
+        private readonly ProjectDianaContext _projectDianaContext;
         private readonly WishGetByIDQuery _testQuery;
+        private readonly WishRecord _testWishRecord;
 
         public WishGetByIDQueryHandlerTests()
         {
             var fixture = new Fixture();
 
-            _projectDianaContext = new Mock<IProjectDianaContext>();
-            _testQuery = fixture.Create<WishGetByIDQuery>();
+            _projectDianaContext = base.InitializeDatabase();
+            _testWishRecord = fixture.Create<WishRecord>();
+            _testQuery = new WishGetByIDQuery(1, _testWishRecord.ID);
 
-            _handler = new WishGetByIDQueryHandler(_projectDianaContext.Object);
+            InitializeDatabase();
+
+            _handler = new WishGetByIDQueryHandler(_projectDianaContext);
         }
 
         [Fact]
         public async Task HandlerRetrievesWish()
         {
+            await InitializeRecords();
+
             var result = await _handler.Handle(_testQuery);
 
             result.Should().NotBeNull();
+        }
+
+        private async Task InitializeRecords()
+        {
+            await _projectDianaContext.WishRecords.AddAsync(_testWishRecord);
+            await _projectDianaContext.SaveChangesAsync();
         }
     }
 }
---
+++ Project.Diana.Tests.Common/TestBases/DbContextTestBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project.Diana.Tests.Common.TestBases
+{
+    public class DbContextTestBase<TContext> where TContext : DbContext
+    {
+        public TContext InitializeDatabase()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<TContext>().UseSqlite(connection).Options;
+
+            using var context = (TContext)Activator.CreateInstance(typeof(TContext), options);
+            context.Database.EnsureCreated();
+
+            return (TContext)Activator.CreateInstance(typeof(TContext), options);
+        }
+    }
+}
