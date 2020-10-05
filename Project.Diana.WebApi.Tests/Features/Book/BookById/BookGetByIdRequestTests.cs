20c62b796c0b90d2309cead69c121f683aad001c
Dennis D. Silva <dsilva609@gmail.com>
2020-10-04 21:43:15 -0400

    feat(book): start get book by id request

---
+++ Project.Diana.WebApi.Tests/Features/Book/BookById/BookGetByIdRequestTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using Project.Diana.Data.Features.User;
+using Project.Diana.WebApi.Features.Book.BookById;
+using Xunit;
+
+namespace Project.Diana.WebApi.Tests.Features.Book.BookById
+{
+    public class BookGetByIdRequestTests
+    {
+        [Theory, AutoData]
+        public void Request_Throws_If_Id_Is_Default(ApplicationUser user)
+        {
+            Action createWithDefaultId = () => new BookGetByIdRequest(0, user);
+
+            createWithDefaultId.Should().Throw<ArgumentException>();
+        }
+
+        [Theory, AutoData]
+        public void Request_Throws_If_Id_Is_Negative(ApplicationUser user)
+        {
+            Action createWithDefaultId = () => new BookGetByIdRequest(-1, user);
+
+            createWithDefaultId.Should().Throw<ArgumentException>();
+        }
+    }
+}
---
+++ Project.Diana.WebApi/Features/Book/BookById/BookGetByIdRequest.cs
@@ -0,0 +1,22 @@
+﻿using Ardalis.GuardClauses;
+using JetBrains.Annotations;
+using MediatR;
+using Project.Diana.Data.Features.Book;
+using Project.Diana.Data.Features.User;
+
+namespace Project.Diana.WebApi.Features.Book.BookById
+{
+    public class BookGetByIdRequest : IRequest<BookRecord>
+    {
+        public int Id { get; }
+        [CanBeNull] public ApplicationUser User { get; }
+
+        public BookGetByIdRequest(int id, ApplicationUser user)
+        {
+            Guard.Against.NegativeOrZero(id, nameof(id));
+
+            Id = id;
+            User = user;
+        }
+    }
+}
