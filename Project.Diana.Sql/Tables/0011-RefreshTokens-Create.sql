21aae45deef77333d5515aac6d8eb12e6902b31b
Dennis D. Silva <dsilva609@gmail.com>
2021-02-11 00:01:22 -0500

    sql: add back refresh token create script

--- Project.Diana.Sql/Tables/0011-RefreshTokens-Create.sql
+++ Project.Diana.Sql/Tables/0011-RefreshTokens-Create.sql
@@ -1,10 +1,17 @@
-﻿CREATE TABLE [dbo].[RefreshTokens]
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[RefreshTokens](
+	[Id] [uniqueidentifier] NOT NULL,
+	[CreatedOn] [datetimeoffset](7) NOT NULL,
+	[ExpiresOn] [datetimeoffset](7) NOT NULL,
+	[Token] [nvarchar](256) NOT NULL,
+	[UserId] [nvarchar](450) NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[RefreshTokens] ADD  CONSTRAINT [PK_RefreshToken] PRIMARY KEY CLUSTERED
 (
-    [Id] UNIQUEIDENTIFIER NOT NULL,
-    [CreatedOn] DATETIMEOFFSET NOT NULL,
-    [ExpiresOn] DATETIMEOFFSET NOT NULL,
-    [Token] NVARCHAR (256) NOT NULL,
-    [UserId] NVARCHAR (450) NOT NULL
-    CONSTRAINT PK_RefreshToken PRIMARY KEY ([Id])
-)
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
